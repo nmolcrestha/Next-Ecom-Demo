56a3c34e2af74c4013a15a0a325290ecaeff0e2d
Anmol Shrestha <nmol.crestha@gmail.com>
2023-03-29 07:03:18 +0545

    adeed failed and success page with cart page and componets

--- ecom_front/components/Header.jsx
+++ ecom_front/components/Header.jsx
@@ -59,21 +59,15 @@ const Header = () => {
           {/* ICON END */}
 
           {/* ICON START */}
+          <Link href='/cart'>
           <div className=" w-8 md:w=12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
             <BsCart className="text-[15px] md:text-[20px]" />
             <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
               5
             </div>
           </div>
-          {/* ICON END */}
-
-          {/* ICON START */}
-          <div className=" w-8 md:w=12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-            <BsCart className="text-[15px] md:text-[20px]" />
-            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-              5
-            </div>
-          </div>
+          </Link>
+          
           {/* ICON END */}
 
           {/* Mobile icon start */}
